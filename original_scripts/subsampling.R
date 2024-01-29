8813cbcc71c033e60bdaab1ff5d6d1f26dd018b9
Fabio de Mello <135765370+fndemello@users.noreply.github.com>
2024-01-29 18:01:38 -0300

    Clean-up of subsampling.R

--- original_scripts/subsampling.R
+++ original_scripts/subsampling.R
@@ -4,18 +4,15 @@ library(doParallel)
 library(foreach)
 options(digits = 15)
 
-setwd("/work2/group/prostata/metodo_normalizacao/")
-
 # Input data
-norm_counts <- read.delim(file='9.Validacao/5.CutnTag/4.Calculos/q_norm_allbins.tsv',header=T,row.names=1)
+norm_counts <- read.delim(file='')
 # Assumes bin count input as described in 'norm+entropy.R'
-
-
 
 
 #####################
 trueSet <- apply(norm_counts,1,function(x) entropy(x,method='ML'))
 trueList <- names(trueSet[order(trueSet, decreasing = T)][1:ceiling(nrow(norm_counts)*0.001)])
+# These are the sets against which similarity and correlation will be compared
 
 # Main test function
 myfun = function(test_counts, fraction, trueSet, trueList){
@@ -34,23 +31,26 @@ trials_fun = function(fraction) {
   removed_percent <- (1-fraction)*100
   return(round(2*removed_percent))
 }
+# This functions controls how many permetutation will be done at each sampling level; since
+# we expect large samplings (eg. 95%) to give fairly consistent results and to also cost a lot
+# of memory to process, this function is inversely proportional to the sampling percentage (so
+# eg. a 10% sampling will be repeated more times than a 90% sampling). The scale of how many
+# tests to run based on this inverse proportion depends on computational resources available
+# (we arbitrarily chose a 2x scaling).
+              
 steps = 0.02
+# Controls the sampling intervals
 
-
+###
 # Make multithreading cluster
-#cl <- makeCluster(clusters)
 registerDoParallel(clusters)
-#clusterExport(cl, varlist = c("trueSet","trueList"))
-#clusterEvalQ(cl, {library(entropy)})
-
+              
 set.seed(0)
 mc.reset.stream()
 preload <- clusters*10
-###
 # To avoid passing the entire counts table to each worker thread, the script pre-samples 
 # up to a set number of permutations at a time, to then be passed to workers. Pre-loading
 # more sets at a time speeds up computation, but consumes more memory.
-###
 
 # MC run
 results <- NULL
@@ -61,14 +61,13 @@ while (p > 0) {
   tests <- NULL
   intermediate_res <- NULL
   trials = trials_fun(p)
-  if (p == 0.50){
-    #stopCluster()
-    #cl <- makeCluster(12)
-    registerDoParallel(24)
-    #clusterExport(cl, varlist = c("trueSet","trueList"))
-    #clusterEvalQ(cl, {library(entropy)})
-  }
-  write(paste('Current: p =',p*100,',',trials,'trials. Start:',start-10800,', update:',Sys.time()-10800),file='6.Calculos/MC_2.cnt_progress.txt',append = T)
+ # if (p == 0.50){
+ #   registerDoParallel(24)
+ # }
+     # Optional QoL, to increase the amount of clusters halfway through; RAM usage, 
+     # usually the limiting factor, tends to fall dramatically as the script goes on.
+ # write(paste('Current: p =',p*100,',',trials,'trials. Start:',start,', update:',Sys.time()),file='',append = T)
+     # Optional QoL, to export updates to disk when running from the CLI.
   while (trials > preload) {
     for (t in 1:preload) {
       tests <- append(tests,list(norm_counts[,sample(1:ncol(norm_counts),ceiling(ncol(norm_counts)*p),replace=F)]))
@@ -89,17 +88,17 @@ while (p > 0) {
   tests <- NULL
   intermediate_res <- NULL
   p <- round(p-steps,2)
-  write.table(results,file='6.Calculos/MC_2.cnt_test.tsv',quote=F,sep='\t', append = F)
 }
 
 end=Sys.time()
 timing = end-start
 timing
-#stopCluster()
 
 colnames(results) <- c('Fraction','Jaccard Index','Correlation')
 
 
-write.table(results,file='6.Calculos/MC_2.cnt_test.tsv',quote=F,sep='\t')
-#write.table(timing,file='6.Calculos/MC_2.mmtest_timer.tsv',quote=F,row.names = F,col.names = F)
+write.table(results,file='')
+  # Final output file
+write.table(timing,file='')
+  # Optional, output of run time, for the sake of optimization
 
